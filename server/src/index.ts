6acca26d8888ca39684d7ef383bc73572d0a0bc0
zakariaorche@gmail.com <zakariaorche@gmail.com>
2025-12-07 17:23:47 +0100

    Megamix Updates

--- server/src/index.ts
+++ server/src/index.ts
@@ -79,7 +79,7 @@ app.use(cors({
   },
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
-  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'],
+  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'x-session-id', 'X-Session-Id'],
   exposedHeaders: ['Content-Length', 'Content-Type'],
 }))
 
